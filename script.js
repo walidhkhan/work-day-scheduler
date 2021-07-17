dd3eefb9f814c7407da117d3a2329480ef72fef2
walidhkhan <walidhkhan@gmail.com>
2021-07-17 14:07:45 -0700

    added ability for text content and content saving to local storage

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+$(document).ready(function(){
 // Displays current date at top of page
-const time = moment().format('dddd, MMMM Do YYYY');
-currentDay.append(time);
+const currentTime = moment().format('dddd, MMMM Do YYYY');
+$('#currentDay').text(currentTime);
+
+// Save btn
+$(".saveBtn").on('click', function(){
+
+    const time = $(this).attr('id');
+    const value = $(this).siblings('textarea').val()
+    console.log(time, value);
+    // stores time & user input to local storage
+    localStorage.setItem(time, value);
+});
+
+
+for (let i = 9; i <= 17; i++) {
+    // for loop with template literal which re-lists user input 
+    $(`#${i}`).siblings('textarea').val(localStorage.getItem(`${i}`));
+};
+
+
+})
+
 
